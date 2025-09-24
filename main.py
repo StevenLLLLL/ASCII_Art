63530a8a63de3c8db6fa2c4c1512fe549436d59b
StevenLLLLL <stevenwnter@gmail.com>
2025-09-24 09:07:10 -0400

    Transfer image to gray image and smaller size.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import pillow
-import opencv
-import tqdm
+from PIL import Image
+import cv2
+from tqdm import tqdm
 import numpy
 
+chars = "　·一二田国黑"
+
+img = Image.open("test.png")
+gray_img = img.convert("L")
+width = 100
+w, h = gray_img.size
+height = int(h * (width / w))
+resized_img = gray_img.resize((width, height))
+
+resized_img.save("resized_test.png")
